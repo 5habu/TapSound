c2e792ffc26e8effaad6342f8e5c6caa0f3fedc2
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-23 23:48:46 +0900

    Initial Commit

---
+++ TapSound/TapSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapSoundApp.swift
+//  TapSound
+//
+//  Created by 5habu on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TapSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
